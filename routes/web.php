6213757d1c83fb4a3c2e2bca622f34f4b8871eb4
Flavio <flavio.origgi@outlook.it>
2021-09-03 17:59:41 +0200

    titolo pagian contatti è ok, stampa contenuto array non va

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+})->name('homepage');
+
+Route::get('/about', function () {
+    $data = [
+        'title' => 'pagina About',
+        'argomento' => 'questa è la pagina ... '
+    ];
+    return view('about', $data);
+})->name('chi-siamo');
+
+Route::get('/contact', function () {
+    $data = [
+        'title' => 'pagina contatti',
+        'nomi' => ['Pippo', 'Pluto', 'Paperino']
+    ];
+    return view('contatti', $data);
+})->name('nostri-contatti');
+
